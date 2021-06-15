851449d7ddf6d8f9cf07abf4ee02a51b4bcebe0e
SoskerWolf <83794523+SoskerWolf@users.noreply.github.com>
2021-06-15 06:41:56 -0500

    Subiendo tablas y base de datos
    La Base de Datos es el archivo hotel_gucci_gucci.sql
    
    Las tablas son cuentas.sql y huespedes.sql
    actualice la tabla huespedes por que tenia registros falsos.

--- SQL/hotel_gucci_gucci.sql
+++ SQL/hotel_gucci_gucci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-06-2021 a las 13:35:43
+-- Tiempo de generación: 15-06-2021 a las 13:36:54
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.7
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `hotel_gucci_gucci`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuentas`
+--
+
+CREATE TABLE `cuentas` (
+  `cuenta` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`cuenta`, `password`) VALUES
+('Carlos', '76d80224611fc919a5d54f0ff9fba446'),
+('Samael', '7815696ecbf1c96e6894b779456d330e');
 
 -- --------------------------------------------------------
 
@@ -58,6 +77,12 @@ INSERT INTO `huespedes` (`Nombre`, `TipoHabitacion`, `NumeroHabitacion`, `Ciudad
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `cuentas`
+--
+ALTER TABLE `cuentas`
+  ADD PRIMARY KEY (`cuenta`);
 
 --
 -- Indices de la tabla `huespedes`
